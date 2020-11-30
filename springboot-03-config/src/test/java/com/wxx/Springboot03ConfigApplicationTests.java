e06ebab70978a0a3aae1ec15ec247bdedfac40e5
SimpleLifeSimpleLove <1137247975@qq.com>
2020-11-30 19:20:03 +0800

    springboot-03-config

---
+++ springboot-03-config/src/test/java/com/wxx/Springboot03ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot03ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
